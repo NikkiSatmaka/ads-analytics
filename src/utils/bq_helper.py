507fb9d10335dfab997d93b1d552a677322cf859
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-07-28 14:20:51 +0700

    feat: update bq_helper

--- src/utils/bq_helper.py
+++ src/utils/bq_helper.py
@@ -6,12 +6,9 @@ from pathlib import Path
 import arrow
 import pandas as pd
 import pandas_gbq
-from dotenv import load_dotenv
 from loguru import logger
 
-from utils.schemas import google_dtypes, tiktok_dtypes
-
-load_dotenv()
+from utils.schemas import google_conversion_dtypes, google_dtypes, tiktok_dtypes
 
 ROOT_DIR = Path(__file__).absolute().parent.parent.parent
 
@@ -19,24 +16,28 @@ ROOT_DIR = Path(__file__).absolute().parent.parent.parent
 def check_existing_bigquery(
     df, project_id, table_id, composite_primary_key
 ) -> pd.DataFrame:
-    if len(composite_primary_key) > 2:
-        date_key, campaign_key, conversion_key = composite_primary_key
+    if len(composite_primary_key) > 3:
+        date_key, customer_key, campaign_key, conversion_key = composite_primary_key
         # Query existing records from BigQuery
         query = f"""
-        SELECT {date_key}, {campaign_key}, {conversion_key}
+        SELECT {date_key}, {customer_key}, {campaign_key}, {conversion_key}
         FROM `{table_id}`
         WHERE {date_key} BETWEEN '{df[date_key].min().strftime('%Y-%m-%d')}' AND '{df[date_key].max().strftime('%Y-%m-%d')}'
         """
-        dtypes = {k: v for k, v in google_dtypes.items() if k in composite_primary_key}
+        dtypes = google_conversion_dtypes
     else:
-        date_key, campaign_key = composite_primary_key
+        date_key, customer_key, campaign_key = composite_primary_key
         # Query existing records from BigQuery
         query = f"""
-        SELECT {date_key}, {campaign_key}
+        SELECT {date_key}, {customer_key}, {campaign_key}
         FROM `{table_id}`
         WHERE {date_key} BETWEEN '{df[date_key].min().strftime('%Y-%m-%d')}' AND '{df[date_key].max().strftime('%Y-%m-%d')}'
         """
-        dtypes = {k: v for k, v in tiktok_dtypes.items() if k in composite_primary_key}
+        if "tiktok" in project_id:
+            dtypes = tiktok_dtypes
+        else:
+            dtypes = google_dtypes
+    dtypes = {k: v for k, v in dtypes.items() if k in composite_primary_key}
     existing_records = pandas_gbq.read_gbq(
         query,
         project_id,
