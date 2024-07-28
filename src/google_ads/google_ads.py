750d148c7980e1091c9c4f496b1ae827d703213f
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-07-28 14:22:38 +0700

    major feat: google ads script split into two table

--- src/google_ads/google_ads.py
+++ src/google_ads/google_ads.py
@@ -4,14 +4,13 @@
 import os
 import sys
 from pathlib import Path
-from typing import Optional
 
 import arrow
 import db_dtypes
+import numpy as np
 import pandas as pd
 import pandas_gbq
 import typer
-from dotenv import load_dotenv
 from google.ads.googleads.client import GoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
 from icecream import ic
@@ -21,9 +20,12 @@ from utils.bq_helper import (
     load_data_to_bigquery,
 )
 from utils.google_ads_helper import get_clients, get_managers
-from utils.schemas import google_dtypes, google_schema
-
-load_dotenv()
+from utils.schemas import (
+    google_conversion_dtypes,
+    google_conversion_schema,
+    google_dtypes,
+    google_schema,
+)
 
 ROOT_DIR = Path(__file__).absolute().parent.parent.parent
 
@@ -40,9 +42,9 @@ QUERY = """
         metrics.impressions,
         metrics.clicks,
         metrics.video_views,
+        metrics.engagements,
         metrics.conversions,
         metrics.all_conversions,
-        metrics.engagements,
         metrics.view_through_conversions,
         metrics.cost_micros,
         metrics.ctr,
@@ -62,10 +64,12 @@ QUERY = """
 QUERY_CONVERSION = """
     SELECT
         segments.date,
+        customer.id,
         campaign.id,
+        campaign.name,
         segments.conversion_action,
-        segments.conversion_action_category,
         segments.conversion_action_name,
+        segments.conversion_action_category,
         metrics.conversions,
         metrics.all_conversions,
         metrics.view_through_conversions
@@ -87,23 +91,24 @@ def get_category_name(lookup_df, category_enum):
     return lookup_df[lookup_df["id"] == category_enum]["category_name"].iloc[0]
 
 
-def get_googleads_query_df(
-    client_id, googleads_service, google_category_lookup, query, query_conversion
-) -> pd.DataFrame:
+def get_googleads_query_df(client_id, googleads_service, query) -> pd.DataFrame:
     response = googleads_service.search(customer_id=client_id, query=query)
     all_reports = []
     for row in response:
         all_reports.append(
             {
                 "date": row.segments.date,
-                "campaign_id": row.campaign.id,
                 "customer_id": row.customer.id,
+                "campaign_id": row.campaign.id,
                 "campaign_name": row.campaign.name,
                 "currency_code": row.customer.currency_code,
                 "impressions": row.metrics.impressions,
                 "clicks": row.metrics.clicks,
                 "video_views": row.metrics.video_views,
                 "engagements": row.metrics.engagements,
+                "conversions": row.metrics.conversions,
+                "all_conversions": row.metrics.all_conversions,
+                "view_through_conversions": row.metrics.view_through_conversions,
                 "cost_micros": row.metrics.cost_micros,
                 "ctr": row.metrics.ctr,
                 "average_cpc": row.metrics.average_cpc,
@@ -112,13 +117,24 @@ def get_googleads_query_df(
                 "cost_per_conversion": row.metrics.cost_per_conversion,
             }
         )
+    if not all_reports:
+        return pd.DataFrame()
+    all_reports = pd.DataFrame(all_reports)
+    return all_reports[google_dtypes.keys()]
+
+
+def get_googleads_query_conversion_df(
+    client_id, googleads_service, google_category_lookup, query_conversion
+) -> pd.DataFrame:
     response = googleads_service.search(customer_id=client_id, query=query_conversion)
     all_reports_conversion = []
     for row in response:
         all_reports_conversion.append(
             {
                 "date": row.segments.date,
+                "customer_id": row.customer.id,
                 "campaign_id": row.campaign.id,
+                "campaign_name": row.campaign.name,
                 "conversion_action": row.segments.conversion_action,
                 "conversion_action_name": row.segments.conversion_action_name,
                 "conversion_action_category": row.segments.conversion_action_category,
@@ -127,39 +143,28 @@ def get_googleads_query_df(
                 "view_through_conversions": row.metrics.view_through_conversions,
             }
         )
-    if not all_reports or not all_reports_conversion:
+    if not all_reports_conversion:
         return pd.DataFrame()
-    all_reports = pd.DataFrame(all_reports)
     all_reports_conversion = pd.DataFrame(all_reports_conversion)
     all_reports_conversion["conversion_action_category"] = all_reports_conversion[
         "conversion_action_category"
     ].apply(lambda x: get_category_name(google_category_lookup, x))
-    all_reports = all_reports.merge(
-        all_reports_conversion,
-        how="outer",
-        on=["date", "campaign_id"],
-    )
-    return all_reports[google_dtypes.keys()]
+    return all_reports_conversion[google_conversion_dtypes.keys()]
 
 
 def get_report_campaign(
     client_id: str,
     googleads_service: GoogleAdsClient,
-    google_category_lookup: pd.DataFrame,
     query: str,
-    query_conversion: str,
     start_date: str,
     end_date: str,
 ) -> pd.DataFrame:
     query = create_query(query, start_date, end_date)
-    query_conversion = create_query(query_conversion, start_date, end_date)
     try:
         report_df = get_googleads_query_df(
             client_id,
             googleads_service,
-            google_category_lookup,
             query,
-            query_conversion,
         )
     except GoogleAdsException as e:
         logger.error(f"Exception when calling GoogleAds API: {e}")
@@ -168,12 +173,40 @@ def get_report_campaign(
         return pd.DataFrame()
     report_df["date"] = pd.to_datetime(report_df["date"])
     report_df["date"] = report_df["date"].astype("dbdate")
-    report_df[["campaign_id", "customer_id"]] = report_df[
-        ["campaign_id", "customer_id"]
+    report_df[["customer_id", "campaign_id"]] = report_df[
+        ["customer_id", "campaign_id"]
     ].astype(str)
     report_df = report_df[report_df["impressions"] > 0].reset_index(drop=True)
-
     return report_df
+
+
+def get_report_campaign_conversion(
+    client_id: str,
+    googleads_service: GoogleAdsClient,
+    google_category_lookup: pd.DataFrame,
+    query_conversion: str,
+    start_date: str,
+    end_date: str,
+) -> pd.DataFrame:
+    query_conversion = create_query(query_conversion, start_date, end_date)
+    try:
+        report_conversion_df = get_googleads_query_conversion_df(
+            client_id,
+            googleads_service,
+            google_category_lookup,
+            query_conversion,
+        )
+    except GoogleAdsException as e:
+        logger.error(f"Exception when calling GoogleAds API: {e}")
+        return pd.DataFrame()
+    if report_conversion_df.empty:
+        return pd.DataFrame()
+    report_conversion_df["date"] = pd.to_datetime(report_conversion_df["date"])
+    report_conversion_df["date"] = report_conversion_df["date"].astype("dbdate")
+    report_conversion_df[["customer_id", "campaign_id"]] = report_conversion_df[
+        ["customer_id", "campaign_id"]
+    ].astype(str)
+    return report_conversion_df
 
 
 @app.command()
@@ -182,10 +215,14 @@ def get_report(date: str, export: bool = False) -> None:
     bq_dataset_id = os.getenv("BIGQUERY_DATASET_ID")
     bq_table_id = os.getenv("BIGQUERY_TABLE_GOOGLE_STAGING_ID")
     bq_table_id = f"{bq_project_id}.{bq_dataset_id}.{bq_table_id}"
+    bq_table_conversion_id = os.getenv("BIGQUERY_TABLE_GOOGLE_CONVERSION_STAGING_ID")
+    bq_table_conversion_id = f"{bq_project_id}.{bq_dataset_id}.{bq_table_conversion_id}"
     bq_category_lookup_id = os.getenv("BIGQUERY_TABLE_GOOGLE_CATEGORY_LOOKUP_ID")
     bq_category_lookup_id = f"{bq_project_id}.{bq_dataset_id}.{bq_category_lookup_id}"
 
     google_category_lookup = pandas_gbq.read_gbq(bq_category_lookup_id, bq_project_id)
+    if google_category_lookup is None:
+        return
 
     start_date = arrow.get(date, tzinfo="local").floor("day")
     end_date = start_date.ceil("day")
@@ -210,18 +247,27 @@ def get_report(date: str, export: bool = False) -> None:
         return
 
     campaign_reports = []
+    conversion_reports = []
     for client_id in clients["client_id"]:
         df_report = get_report_campaign(
             client_id,
             googleads_service,
-            google_category_lookup,
             QUERY,
-            QUERY_CONVERSION,
             start_date.format("YYYY-MM-DD"),
             end_date.format("YYYY-MM-DD"),
         )
         if not df_report.empty:
             campaign_reports.append(df_report)
+        df_report_conversion = get_report_campaign_conversion(
+            client_id,
+            googleads_service,
+            google_category_lookup,
+            QUERY_CONVERSION,
+            start_date.format("YYYY-MM-DD"),
+            end_date.format("YYYY-MM-DD"),
+        )
+        if not df_report_conversion.empty:
+            conversion_reports.append(df_report_conversion)
 
     if campaign_reports:
         df_final = pd.concat(campaign_reports, axis=0)
@@ -229,17 +275,35 @@ def get_report(date: str, export: bool = False) -> None:
             export_to_parquet(
                 df_final,
                 "google",
-                ROOT_DIR / f"data_lake/google_ads/{year}/{month}/{day}",
+                ROOT_DIR / f"data_lake/google_ads/campaign/{year}/{month}/{day}",
             )
         load_data_to_bigquery(
             df_final,
             bq_project_id,
             bq_table_id,
             google_schema,
-            ("date", "campaign_id", "conversion_action"),
+            ("date", "customer_id", "campaign_id"),
         )
     else:
         logger.info("No campaign reports found.")
+
+    if conversion_reports:
+        df_conversion_final = pd.concat(conversion_reports, axis=0)
+        if export:
+            export_to_parquet(
+                df_conversion_final,
+                "google_conversion",
+                ROOT_DIR / f"data_lake/google_ads/conversion_goal/{year}/{month}/{day}",
+            )
+        load_data_to_bigquery(
+            df_conversion_final,
+            bq_project_id,
+            bq_table_conversion_id,
+            google_conversion_schema,
+            ("date", "customer_id", "campaign_id", "conversion_action"),
+        )
+    else:
+        logger.info("No conversion reports found.")
 
 
 if __name__ == "__main__":
